05b6d5611253fddcfaab80d688d15709f6d4f4bc
Zack Stevens <zack.st7@gmail.com>
2021-12-22 09:56:37 -0700

    fix: Testing

--- helm.js
+++ helm.js
@@ -7,24 +7,24 @@ const charts = JSON.parse(parsedArgs.charts)
 const repo = parsedArgs.repo
 
 charts.forEach(chart => {
-    cmd = `helm install ${chart.release_name}`
-    if (chart.namespace) {
-        cmd += ` -n ${chart.namespace}`
-    }
+    // cmd = `helm install ${chart.release_name}`
+    cmd = `helm template`
+    // if (chart.namespace) {
+    //     cmd += ` -n ${chart.namespace}`
+    // }
     cmd += ` ${repo}/${chart.name} --version ${chart.version}`
     if (chart.values) {
         cmd += ` -f ${chart.values}`
     }
-    console.log(`cmd is ${cmd}`)
+    exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
 })
-// exec("ls -la", (error, stdout, stderr) => {
-//     if (error) {
-//         console.log(`error: ${error.message}`);
-//         return;
-//     }
-//     if (stderr) {
-//         console.log(`stderr: ${stderr}`);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout}`);
-// });
