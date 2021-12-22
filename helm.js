169cf9463b66888a080dfd2a3fb09903c0f3572f
Zack Stevens <zack.st7@gmail.com>
2021-12-22 08:40:15 -0700

    fix: Testing

--- helm.js
+++ helm.js
@@ -1,1 +1,13 @@
-console.log('hooray')
+const { exec } = require("child_process");
+
+exec("ls -la", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
