e83fcaf9aba999519b1c7339ac7a4a79648d970f
Zack Stevens <zack.st7@gmail.com>
2021-12-24 09:51:02 -0700

    fix: Testing

--- run-dependencies.sh
+++ run-dependencies.sh
@@ -7,7 +7,11 @@ mkdir dependencies && cd dependencies
 
 for i in ${dependencies//,/ }
 do
-  git clone https://${GITHUB_TOKEN}@github.com/swarm-io/event-stream-coordinator.git
+  git clone https://${GITHUB_TOKEN}@github.com/swarm-io/${i}.git
   cd $i
-  skaffold run --port-forward=user --verbosity=info > ${GITHUB_WORKSPACE}/skaffold-logs/$i.txt &
+  echo "will log to ${GITHUB_WORKSPACE}/skaffold-logs/${i}.txt"
+  echo "stuff" > ${GITHUB_WORKSPACE}/skaffold-logs/${i}.txt
+  cat ${GITHUB_WORKSPACE}/skaffold-logs/${i}.txt
+  echo "stuff" > ${GITHUB_WORKSPACE}/skaffold-logs/${i}.txt
+  skaffold run --port-forward=user --verbosity=info > ${GITHUB_WORKSPACE}/skaffold-logs/${i}.txt &
 done
