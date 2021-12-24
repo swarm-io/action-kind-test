042bbdd65afc967495a07d901f963807e094aaaa
Zack Stevens <zack.st7@gmail.com>
2021-12-24 09:42:42 -0700

    fix: Testing

--- run-dependencies.sh
+++ run-dependencies.sh
@@ -9,8 +9,5 @@ for i in ${dependencies//,/ }
 do
   git clone https://${GITHUB_TOKEN}@github.com/swarm-io/event-stream-coordinator.git
   cd $i
-  pwd
-  ls
-#  skaffold run --port-forward=user --verbosity=info > ${GITHUB_WORKSPACE}/skaffold-logs/$i.txt &
-  skaffold run --verbosity=info
+  skaffold run --port-forward=user --verbosity=info > ${GITHUB_WORKSPACE}/skaffold-logs/$i.txt &
 done
